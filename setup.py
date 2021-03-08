8b59d075ba0b319167e64ff67672d44255596222
marqrazz <marq.razz@gmail.com>
2021-03-08 14:22:19 -0700

    ported to ros2 python package

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+# from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
 
-d = generate_distutils_setup(
+setup(
     packages=['rosdoc_lite'],
     package_dir={'': 'src'},
     scripts=['scripts/rosdoc_lite'],
     package_data={'rosdoc_lite': ['templates/*']}
 )
 
-setup(**d)
+# setup(**d)
