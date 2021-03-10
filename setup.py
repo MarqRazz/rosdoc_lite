714fc1f07ac431f4c19853443ca83baa1691fd4e
marqrazz <marq.razz@gmail.com>
2021-03-10 08:28:51 -0700

    commented out MANIFEST.in include

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+# from distutils.core import setup
 # from catkin_pkg.python_setup import generate_distutils_setup
 from setuptools import setup
 
